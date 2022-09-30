d3f90fb0f89fdb4c9e7d9b5ea17b0a09f51240cb
muyangit <muyangit@outlook.com>
2022-09-30 13:16:42 +0800

    auto update

--- 博客备份.bat
+++
@@ -1,9 +0,0 @@
-@echo OFF
-::重装系统后注意两处盘符的修改
-G:
-G:\Demo_Git\pic_url\
-start D:\MySoftware\DEV\VersionCtrl\Git\git-bash.exe -c "pic_url_backup.sh"
-git add .
-git commit -m "From Auto Updata"
-git push -u origin master -f 
-git pull –rebase origin master
--- 图床备份_gitbash.bat
+++
@@ -1,5 +0,0 @@
-@echo OFF
-::重装系统后注意两处盘符的修改
-G:
-G:\Demo_Git\pic_url\
-start D:\MySoftware\DEV\VersionCtrl\Git\git-bash.exe -c "bash pic_url_backup.sh"
